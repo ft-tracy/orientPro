904755455ea0d9818b3deee738743e6bc42c72db
Angela Jeptoo <98097828+AJ-1567@users.noreply.github.com>
2024-05-24 09:09:36 +0300

    added database helper and dummy page

---
+++ main/java/com/example/orientpro/DummyHomePageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.orientpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DummyHomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dummy_home_page)
+    }
+}
