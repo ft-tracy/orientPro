1f56004cd6376d1bdd0ddd7896e50af94c26a9bf
Agina06 <brianagina1906@gmail.com>
2024-05-24 14:24:19 +0300

    Update User.cs

--- Data/User.cs
+++ Data/User.cs
@@ -1,15 +1,30 @@
-﻿namespace LoginApp.Models
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginApp.Models
 {
     public class User
     {
+        [Key]
         public int UniqueID { get; set; }
-        public string Name { get; set; }
-        public string Username { get; set; }
-        public string Pwd { get; set; }
-        public string Email { get; set; }
-        public string Role { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string Pwd { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
         public DateTime DateJoined { get; set; }
+
         public bool HasAccess { get; set; }
+
         public bool IsFirstLogin { get; set; }
+
+        public string OTP { get; set; }  // Add this line
+
+        public DateTime OTPExpiration { get; set; }  // Add this line
+        public string LastName { get; set; } = string.Empty;
     }
 }
