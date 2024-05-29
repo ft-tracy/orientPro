e993491312976d08c82bd92ff52e2274d06542f8
Agina06 <brianagina1906@gmail.com>
2024-05-29 14:07:39 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 
 namespace LoginApp
 {
@@ -21,16 +25,35 @@ namespace LoginApp
                         var certPath = certConfig.GetValue<string>("Path");
                         var certPassword = certConfig.GetValue<string>("Password");
 
+                        var port = GetAvailablePort(5000, 5100); // Check ports between 5000 and 5100
+
                         if (!string.IsNullOrEmpty(certPath) && !string.IsNullOrEmpty(certPassword))
                         {
-                            serverOptions.ListenAnyIP(44308, listenOptions =>
+                            serverOptions.ListenAnyIP(port, listenOptions =>
                             {
                                 listenOptions.UseHttps(certPath, certPassword);
                             });
                         }
                     });
                     webBuilder.UseStartup<Startup>()
-                              .UseUrls("https://0.0.0.0:44308");
+                              .UseUrls("https://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT") ?? "5000");
                 });
+
+        private static int GetAvailablePort(int minPort, int maxPort)
+        {
+            var usedPorts = IPGlobalProperties.GetIPGlobalProperties()
+                .GetActiveTcpListeners()
+                .Select(p => p.Port)
+                .ToArray();
+
+            for (int port = minPort; port <= maxPort; port++)
+            {
+                if (!usedPorts.Contains(port))
+                {
+                    return port;
+                }
+            }
+            throw new Exception("No available ports in the specified range.");
+        }
     }
 }
