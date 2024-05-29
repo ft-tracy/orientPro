7c923f53703d6c71c5f44f4caa7f15459cf25faa
Agina06 <brianagina1906@gmail.com>
2024-05-29 12:55:01 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,43 +18,26 @@ namespace LoginApp
                 {
                     webBuilder.ConfigureKestrel((context, serverOptions) =>
                     {
-                        var kestrelConfig = context.Configuration.GetSection("Kestrel:Endpoints");
-
-                        // Read HTTP endpoint configuration
-                        var httpEndpoint = kestrelConfig.GetSection("Http:Url").Value;
-                        if (!string.IsNullOrEmpty(httpEndpoint))
-                        {
-                            serverOptions.ListenAnyIP(new Uri(httpEndpoint).Port);
-                        }
-
-                        // Read HTTPS endpoint configuration
-                        var httpsEndpoint = kestrelConfig.GetSection("Https:Url").Value;
-                        var certConfig = kestrelConfig.GetSection("Https:Certificate");
+                        var certConfig = context.Configuration.GetSection("Kestrel:Endpoints:Https:Certificate");
                         var certPath = certConfig.GetValue<string>("Path");
                         var certPassword = certConfig.GetValue<string>("Password");
 
-                        if (!string.IsNullOrEmpty(httpsEndpoint) && !string.IsNullOrEmpty(certPath) && !string.IsNullOrEmpty(certPassword))
+                        if (!string.IsNullOrEmpty(certPath) && !string.IsNullOrEmpty(certPassword))
                         {
-                            serverOptions.ListenAnyIP(new Uri(httpsEndpoint).Port, listenOptions =>
+                            serverOptions.ConfigureHttpsDefaults(listenOptions =>
                             {
-                                listenOptions.UseHttps(certPath, certPassword);
+                                listenOptions.ServerCertificate = new X509Certificate2(certPath, certPassword);
                             });
                         }
                     });
 
-                    // Check if Render environment variable exists
+                    // Use the port specified by Render, otherwise default ports will be used
                     var renderPort = Environment.GetEnvironmentVariable("PORT");
-                    if (!string.IsNullOrEmpty(renderPort) && int.TryParse(renderPort, out int port))
+                    if (!string.IsNullOrEmpty(renderPort))
                     {
-                        // Use the port specified by Render
-                        webBuilder.UseUrls($"http://0.0.0.0:{port}");
+                        webBuilder.UseUrls($"http://0.0.0.0:{renderPort}");
                     }
-                    else
-                    {
-                        // Use a default port if not running in Render
-                        webBuilder.UseUrls("http://0.0.0.0:5000"); // Updated to use port 5000
-                    }
-
+                    
                     webBuilder.UseStartup<Startup>();
                 });
     }
