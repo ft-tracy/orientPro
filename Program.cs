4a18c15ad079ac3b76c40ac56110dfb7dcf6ddfb
Agina06 <brianagina1906@gmail.com>
2024-05-29 15:59:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace LoginApp
@@ -17,15 +16,13 @@ namespace LoginApp
                 {
                     webBuilder.ConfigureKestrel((context, serverOptions) =>
                     {
-                        var certConfig = context.Configuration.GetSection("Kestrel:Endpoints:Https:Certificate");
-                        var certPath = certConfig.GetValue<string>("Path");
-                        var certPassword = certConfig.GetValue<string>("Password");
+                        var url = context.Configuration["Kestrel:Endpoints:Https:Url"];
 
-                        if (!string.IsNullOrEmpty(certPath) && !string.IsNullOrEmpty(certPassword))
+                        if (!string.IsNullOrEmpty(url))
                         {
-                            serverOptions.ListenAnyIP(0, listenOptions =>
+                            serverOptions.ListenAnyIP(443, listenOptions =>
                             {
-                                listenOptions.UseHttps(certPath, certPassword);
+                                listenOptions.UseHttps();
                             });
                         }
                     });
