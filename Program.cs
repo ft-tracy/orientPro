e90d5ca8b55fef1ba0e9906cee9f673c2f8fa90b
Agina06 <brianagina1906@gmail.com>
2024-05-30 09:19:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace LoginApp
 {
@@ -9,35 +8,18 @@ namespace LoginApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // Read the port number from the PORT environment variable
+            string port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+
+            CreateHostBuilder(args, port).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args, string port) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.ConfigureKestrel((context, serverOptions) =>
-                    {
-                        var certConfig = context.Configuration.GetSection("Kestrel:Endpoints:Https:Certificate");
-                        var certPath = certConfig.GetValue<string>("Path");
-                        var certPassword = certConfig.GetValue<string>("Password");
-
-                        if (!string.IsNullOrEmpty(certPath) && !string.IsNullOrEmpty(certPassword))
-                        {
-                            serverOptions.ListenAnyIP(int.Parse(GetPort()), listenOptions =>
-                            {
-                                listenOptions.UseHttps(certPath, certPassword);
-                            });
-                        }
-                    });
                     webBuilder.UseStartup<Startup>()
-                              .UseUrls("https://0.0.0.0:" + GetPort());
+                              .UseUrls($"http://0.0.0.0:{port}");
                 });
-
-        private static string GetPort()
-        {
-            var port = Environment.GetEnvironmentVariable("PORT");
-            return string.IsNullOrEmpty(port) ? "5000" : port;
-        }
     }
 }
