ecbec91665e396a8b26ee9550d0c6b46c6112b30
Angela Jeptoo <98097828+AJ-1567@users.noreply.github.com>
2024-06-05 15:25:40 +0300

    Update LoginActivity.kt

--- main/java/com/example/orientpro/LoginActivity.kt
+++ main/java/com/example/orientpro/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.example.orientpro
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.orientpro.utils.APIService
+import com.example.orientpro.repository.AuthRepository
+import com.example.orientpro.utils.Result
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,6 +16,8 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var etPassword: EditText
     private lateinit var btnLoginHome: Button
     private lateinit var btnBack: ImageButton
+
+    private lateinit var authRepository: AuthRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,14 +28,37 @@ class LoginActivity : AppCompatActivity() {
         btnLoginHome = findViewById(R.id.btnLoginHome)
         btnBack = findViewById(R.id.btnBack)
 
+        authRepository = AuthRepository(this)
+
         btnLoginHome.setOnClickListener {
             val email = etEmailOrUsername.text.toString()
             val password = etPassword.text.toString()
-            APIService.login(this, email, password)  // Call the login method from APIService
+            loginUser(email, password)
         }
 
         btnBack.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        authRepository.login(email, password) { result ->
+            when (result) {
+                is Result.Loading -> {
+                    // Show loading indicator if necessary
+                }
+                is Result.Success -> {
+                    // Navigate to HomepageActivity
+                    val intent = Intent(this, HomepageActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                }
+                is Result.Error -> {
+                    // Show error message
+                    Toast.makeText(this, "Login failed: ${result.exception.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
