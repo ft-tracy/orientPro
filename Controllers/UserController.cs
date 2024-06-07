ed84040d6846b6d9632bb3c599dd5916db963f0f
Agina06 <brianagina1906@gmail.com>
2024-06-07 15:24:31 +0300

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginApp.Services;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace LoginApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetUser()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = _userService.GetUserById(userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
