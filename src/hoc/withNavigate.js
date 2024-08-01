47ccf9088ace9c9f9955752e514412faad642c7e
Jeanne Wanjiru <71101608+Jeanne21@users.noreply.github.com>
2024-08-01 09:56:36 +0300

    Delete src directory

--- src/hoc/withNavigate.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
-export const withNavigate = (Component) => {
-  return (props) => {
-    const navigate = useNavigate();
-    return <Component {...props} navigate={navigate} />;
-  };
-};
